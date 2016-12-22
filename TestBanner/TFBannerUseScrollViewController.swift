5c1a8c6addf360ed7d5f5a6d12233e695f92bee3
田腾飞 <tiantengfei@hujiang.com>
2016-12-22 17:05:06 +0800

    实现分页

--- TestBanner/TFBannerUseScrollViewController.swift
+++ TestBanner/TFBannerUseScrollViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class TFBannerUseScrollViewController: UIViewController {
 
+    let imageCount = 4
+    var scrollView: UIScrollView!
+    var pageView: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupViews()
     }
     
+    func setupViews() {
+        do {
+            scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: kScreenWidth, height: kScreenHeight))
+            view.addSubview(scrollView)
+        }
+        
+        do {
+            pageView = UIPageControl(frame: CGRect(x: 0, y: kScreenHeight - 30, width: kScreenWidth, height: 30))
+            view.addSubview(pageView)
+            pageView.numberOfPages = imageCount
+            pageView.currentPage = 0
+            pageView.pageIndicatorTintColor = UIColor.white
+            pageView.currentPageIndicatorTintColor = UIColor.blue
+        }
+        
+        do {
+            for index in 0..<imageCount {
+                let imageView = UIImageView(frame: CGRect(x: CGFloat(index) * kScreenWidth, y: 0, width: kScreenWidth, height: kScreenHeight))
+                imageView.image = UIImage(named: "\(index + 1).png")
+                scrollView.addSubview(imageView)
+            }
+        }
+        
+        do {
+            scrollView.contentSize = CGSize(width: kScreenWidth * CGFloat(scrollView.subviews.count), height: 0)
+            scrollView.isPagingEnabled = true
+            scrollView.showsHorizontalScrollIndicator = false
+        }
+    }
 }
