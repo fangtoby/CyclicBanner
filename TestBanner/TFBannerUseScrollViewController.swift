fb7d59ab97d711c95cad65119f9157d066074e48
田腾飞 <tiantengfei@hujiang.com>
2016-12-22 15:43:49 +0800

    初始化项目

---
+++ TestBanner/TFBannerUseScrollViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TFBannerUseScrollViewController.swift
+//  TestBanner
+//
+//  Created by tiantengfei on 2016/12/22.
+//  Copyright © 2016年 田腾飞. All rights reserved.
+//
+
+import UIKit
+
+class TFBannerUseScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
