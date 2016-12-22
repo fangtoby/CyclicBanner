476793068ccd5b541ac574ca5cecf0c9cd1551d5
田腾飞 <tiantengfei@hujiang.com>
2016-12-22 17:35:04 +0800

    实现轮播

--- TestBanner/TFBannerUseScrollViewController.swift
+++ TestBanner/TFBannerUseScrollViewController.swift
@@ -13,15 +13,18 @@ class TFBannerUseScrollViewController: UIViewController {
     let imageCount = 4
     var scrollView: UIScrollView!
     var pageView: UIPageControl!
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
+        addTimer()
     }
     
     func setupViews() {
         do {
             scrollView = UIScrollView(frame: CGRect(x: 0, y: 0, width: kScreenWidth, height: kScreenHeight))
+            scrollView.delegate = self
             view.addSubview(scrollView)
         }
         
@@ -47,5 +50,39 @@ class TFBannerUseScrollViewController: UIViewController {
             scrollView.isPagingEnabled = true
             scrollView.showsHorizontalScrollIndicator = false
         }
+    }
+    
+    func addTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.5, repeats: true, block: { [weak self] (timer) in
+            self?.nextImage()
+        })
+    }
+    
+    func nextImage() {
+        if pageView.currentPage == imageCount - 1 {
+            pageView.currentPage = 0
+        } else {
+            pageView.currentPage += 1
+        }
+        let contentOffset = CGPoint(x: kScreenWidth * CGFloat(pageView.currentPage), y: 0)
+        scrollView.setContentOffset(contentOffset, animated: true)
+    }
+}
+
+extension TFBannerUseScrollViewController: UIScrollViewDelegate {
+    
+    /// 开始滑动的时候，停止timer
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        
+    }
+    
+    /// 滑动过程中改变page
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+    }
+    
+    /// 停止滑动，开始timer
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        
     }
 }
