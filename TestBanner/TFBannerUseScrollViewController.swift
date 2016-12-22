1aaf4ad0ac97bcfdb4bd2c4df355594c010ec607
田腾飞 <tiantengfei@tianteneideMBP4.lan>
2016-12-22 22:49:45 +0800

    使用UIScrollView完美实现图片轮播器

--- TestBanner/TFBannerUseScrollViewController.swift
+++ TestBanner/TFBannerUseScrollViewController.swift
@@ -38,51 +38,85 @@ class TFBannerUseScrollViewController: UIViewController {
         }
         
         do {
-            for index in 0..<imageCount {
+            for index in 0..<3 {
                 let imageView = UIImageView(frame: CGRect(x: CGFloat(index) * kScreenWidth, y: 0, width: kScreenWidth, height: kScreenHeight))
-                imageView.image = UIImage(named: "\(index + 1).png")
+                imageView.image = UIImage(named: "\((index + 3) % 4).png")
                 scrollView.addSubview(imageView)
             }
         }
         
         do {
-            scrollView.contentSize = CGSize(width: kScreenWidth * CGFloat(scrollView.subviews.count), height: 0)
+            scrollView.contentSize = CGSize(width: kScreenWidth * 3, height: 0)
+            scrollView.contentOffset = CGPoint(x: kScreenWidth, y: 0)
             scrollView.isPagingEnabled = true
             scrollView.showsHorizontalScrollIndicator = false
+            scrollView.showsVerticalScrollIndicator = false
         }
     }
     
+    /// 添加timer
     func addTimer() {
-        timer = Timer.scheduledTimer(withTimeInterval: 1.5, repeats: true, block: { [weak self] (timer) in
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { [weak self] (timer) in
             self?.nextImage()
         })
     }
     
+    /// 下一个图片
     func nextImage() {
         if pageView.currentPage == imageCount - 1 {
             pageView.currentPage = 0
         } else {
             pageView.currentPage += 1
         }
-        let contentOffset = CGPoint(x: kScreenWidth * CGFloat(pageView.currentPage), y: 0)
+        let contentOffset = CGPoint(x: kScreenWidth * 2, y: 0)
         scrollView.setContentOffset(contentOffset, animated: true)
+    }
+    
+    /// 上一个图片
+    func preImage() {
+        if pageView.currentPage == 0 {
+            pageView.currentPage = imageCount - 1
+        } else {
+            pageView.currentPage -= 1
+        }
+        
+        let contentOffset = CGPoint(x: 0, y: 0)
+        scrollView.setContentOffset(contentOffset, animated: true)
+    }
+    
+    /// 重新加载图片，重新设置3个imageView
+    func reloadImage() {
+        let currentIndex = pageView.currentPage
+        let nextIndex = (currentIndex + 1) % 4
+        let preIndex = (currentIndex + 3) % 4
+        
+        (scrollView.subviews[0] as! UIImageView).image = UIImage(named: "\(preIndex).png")
+        (scrollView.subviews[1] as! UIImageView).image = UIImage(named: "\(currentIndex).png")
+        (scrollView.subviews[2] as! UIImageView).image = UIImage(named: "\(nextIndex).png")
     }
 }
 
 extension TFBannerUseScrollViewController: UIScrollViewDelegate {
     
-    /// 开始滑动的时候，停止timer
+    /// 开始滑动的时候，停止timer，设置为niltimer才会销毁
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        
+        timer?.invalidate()
+        timer = nil
     }
     
-    /// 滑动过程中改变page
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        reloadImage()
+        scrollView.setContentOffset(CGPoint(x: kScreenWidth, y: 0), animated: false)
     }
     
-    /// 停止滑动，开始timer
+    /// 停止拖拽，开始timer, 并且判断是显示上一个图片还是下一个图片
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        addTimer()
         
+        if scrollView.contentOffset.x < kScreenWidth {
+            preImage()
+        } else {
+            nextImage()
+        }
     }
 }
