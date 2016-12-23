f0a51b46e02ab04c5c2523714d6c41e5711d46cb
田腾飞 <tiantengfei@outlook.com>
2016-12-23 22:55:06 +0800

    使用UICollection完美实现循环轮播

--- TestBanner/TFBannerUseCollectionViewController.swift
+++ TestBanner/TFBannerUseCollectionViewController.swift
@@ -14,7 +14,8 @@ class TFBannerUseCollectionViewController: UIViewController {
     var collectionView: UICollectionView!
     var pageView: UIPageControl!
     var timer: Timer?
-    
+    var currentIndexPath: IndexPath?
+    var oldOffset: CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +40,11 @@ class TFBannerUseCollectionViewController: UIViewController {
             collectionView.dataSource = self
             collectionView.isPagingEnabled = true
             collectionView.showsHorizontalScrollIndicator = false
+            
+            //从中间显示
+            let indexPath = IndexPath(item: images.count, section: 0)
+            currentIndexPath = indexPath
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
         }
         
         do {
@@ -70,7 +76,48 @@ class TFBannerUseCollectionViewController: UIViewController {
     
     /// 下一个图片
     func nextImage() {
+        if pageView.currentPage == images.count - 1 {
+            pageView.currentPage = 0
+        } else {
+            pageView.currentPage += 1
+        }
         
+        guard let currentIndexPath = currentIndexPath else {
+            return
+        }
+        let newIndexPath = IndexPath(item: currentIndexPath.item + 1, section: 0)
+        self.currentIndexPath = newIndexPath
+        collectionView.scrollToItem(at: newIndexPath, at: .centeredHorizontally, animated: true)
+    }
+    
+    func preImage() {
+        if pageView.currentPage == 0 {
+            pageView.currentPage = images.count - 1
+        } else {
+            pageView.currentPage -= 1
+        }
+        
+        guard let currentIndexPath = currentIndexPath else {
+            return
+        }
+        let newIndexPath = IndexPath(item: currentIndexPath.item - 1, section: 0)
+        self.currentIndexPath = newIndexPath
+        collectionView.scrollToItem(at: newIndexPath, at: .centeredHorizontally, animated: true)
+    }
+    
+    func reloadImage() {
+        guard let currentIndexPath = currentIndexPath else {
+            return
+        }
+        if currentIndexPath.item == images.count * 2 - 1 {  //如果是最后一个图片，回到前一个section的最后一张图片
+            let newIndexPath = IndexPath(item: images.count - 1, section: 0)
+            self.currentIndexPath = newIndexPath
+            collectionView.scrollToItem(at: newIndexPath, at: .centeredHorizontally, animated: false)
+        } else if currentIndexPath.item == 0 {  //如果是第一个图片，就回到第二个section的第一张图片
+            let newIndexPath = IndexPath(item: images.count, section: 0)
+            self.currentIndexPath = newIndexPath
+            collectionView.scrollToItem(at: newIndexPath, at: .centeredHorizontally, animated: false)
+        }
     }
 }
 
@@ -80,13 +127,12 @@ extension TFBannerUseCollectionViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images.count * 3
+        return images.count * 2
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? TFBannerCollectionViewCell
         
-        print(indexPath.row)
         if let cell = cell {
             cell.imageName = images[indexPath.item % 4]
             return cell
@@ -97,5 +143,27 @@ extension TFBannerUseCollectionViewController: UICollectionViewDataSource {
 }
 
 extension TFBannerUseCollectionViewController: UICollectionViewDelegate {
+    
+    /// 开始滑动的时候，停止timer，设置为niltimer才会销毁
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        removeTimer()
+        oldOffset = scrollView.contentOffset.x
+    }
+    
+    /// 当停止滚动的时候重新设置三个ImageView的内容，然后悄悄滴显示中间那个imageView
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        reloadImage()
+    }
+    
+    /// 停止拖拽，开始timer, 并且判断是显示上一个图片还是下一个图片
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        addTimer()
+        
+        if scrollView.contentOffset.x < oldOffset {
+            preImage()
+        } else {
+            nextImage()
+        }
+    }
     
 }
